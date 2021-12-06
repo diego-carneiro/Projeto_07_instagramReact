fc0bd65855ed66d1a561a82815c20f79327d0d01
diego-carneio <diegosantos.cs@gmail.com>
2021-12-06 16:06:37 -0300

    rebuild fisinhed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useState } from "react";
-import Main_page from "./components/structural/Main_page";
-import Login_page from "./views/Login";
-import Signup_page from "./views/Signup";
-import Header from "./components/structural/Header";
-import Story_box from "./components/feed_components/Stories";
 import Page_body from "./components/structural/Page_body";
 
 export default function App() {
